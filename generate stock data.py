38f0d4d1ee40647e8392c8810f9834bf87f06cc0
Ayan Gupta <ayangupta654@gmail.com>
2026-06-02 17:46:36 +0530

    feat: add trade event producer with anomaly simulation

---
+++ generate stock data.py
@@ -0,0 +1,39 @@
+import json
+import random
+from datetime import datetime
+import time
+tickers = ['INFY', 'HDFCBANK', 'RELIANCE', 'TCS', 'ICICIBANK', 'HINDUNILVR', 'KOTAKBANK', 'SBIN', 'ITC', 'LT']
+base_price = {
+    'INFY': 1500,
+    'HDFCBANK': 1200,
+    'RELIANCE': 2000,
+    'TCS': 3000,
+    'ICICIBANK': 800,
+    'HINDUNILVR': 2500,
+    'KOTAKBANK': 1800,
+    'SBIN': 500,
+    'ITC': 1000,
+    'LT': 2200
+}
+
+def generate_stock_data():
+        ticker = random.choice(tickers)
+        price = base_price[ticker] + random.uniform(-50, 50)
+        if random.random() < 0.05:
+            volume = random.randint(50000,200000)
+        else:
+            volume = random.randint(1000, 10000)
+        timestamp = datetime.now().isoformat()
+        trade_type = random.choice(['BUY','SELL'])
+        return({
+            'ticker': ticker,
+            'price': round(price, 2),
+            'volume': volume,
+            'timestamp': timestamp,
+            'trade_type': trade_type
+            })
+while True:
+    trade = generate_stock_data()
+    print(json.dumps(trade))
+    time.sleep(1) 
+# print(json.dumps(generate_stock_data()))
